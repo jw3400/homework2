2168be12c18cf76b3da788a806cc4e2d8f83c9ad
ChoiJinwon <jw3400@gmail.com>
2016-09-29 00:12:20 +0900

    A01.java 계속 수정종

--- A01.java
+++ A01.java
@@ -3,7 +3,7 @@ import java.util.*;
 class A01 {
 	void input(){
 		Scanner s = new Scanner(System.in);
-		System.out.print("태어난 년도를 입력하세요.");
+		System.out.print("태어난 년도를 입력하세요. ");
 		int birth_year = s.nextInt();
 		int age = cal(birth_year);
 	
@@ -11,21 +11,22 @@ class A01 {
 
 	int cal(int birth_year){
 		int age = 0;
-		age = 2014 - birth_year + 1;
+		age = 2016 - birth_year + 1;
 		return age;
 	}
 
-	void print(){
-		if(int age > 20){
+	void print(int age){
+		if(age > 20)
 			System.out.print("미성년자가 아닙니다.");
-		}
-		else {
+		
+		else 
 			System.out.print("미성년자 입니다.");
-		}
+		
 	}
 
 	public static void main(String[] args){
 		new A01().input();
+		new A01().print(int age);
 	}
 
 
