a97a890218f76f8f424ead042a6417739c18b616
ChoiJinwon <jw3400@gmail.com>
2016-09-29 19:50:23 +0900

    A02.java 문제해결

--- A02.java
+++ A02.java
@@ -4,12 +4,15 @@ class A02 {
 	void input(){
 		Scanner s = new Scanner(System.in);
 		System.out.print("온도를 입력하세요. ");
-		double input_degree = s.nextdouble();
+		double input_degree = s.nextDouble();	
+
 		System.out.print("입력하신 온도가 섭씨온도이면 C를, 화씨온도이면 F를 입력하세요. ");
-		String kind = s.nextString();
-		double output_degree = cal(input_degree); 	//섭씨온도로 계산
+		String kind = s.next();
+
+		double output_degree = cal(input_degree);	//섭씨온도로 계산
 		double output_degree2 = cal2(input_degree);	//화씨온도로 계산
-		print(kind);
+
+		print(kind, output_degree, output_degree2);
 
 	}
 
@@ -27,13 +30,17 @@ class A02 {
 	}
 
 	void print(String kind, double output_degree, double output_degree2){
-		if(kind == "C" || kind == "c")	//입력된 온도가 섭씨온도로 지정하면 화씨온도로 변환
+		if(kind.equalsIgnoreCase("C"))	//입력된 온도가 섭씨온도로 지정하면 화씨온도로 변환
+		{	
 			System.out.print("변환된 온도는 " + output_degree2 + " 입니다.");		
-		else if(kind == "F" || kind == "f")  //입력된 온도가 화씨온도로 지정하면 섭씨온도로 변환
+		}
+		else if(kind.equalsIgnoreCase("F"))  //입력된 온도가 화씨온도로 지정하면 섭씨온도로 변환
+		{	
 			System.out.print("변환된 온도는 " + output_degree + " 입니다.");	
+		}
 		else
 			System.out.print("섭씨온도 또는 화씨온도 기호가 아닙니다.");
-	}
+	} 
 
 	public static void main(String[] args){
 
