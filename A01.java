fcf9fdd6ef791d4a85be466783cc82d3a7310d23
ChoiJinwon <jw3400@gmail.com>
2016-09-29 10:54:05 +0900

    A01.java 문제해결

--- A01.java
+++ A01.java
@@ -6,27 +6,29 @@ class A01 {
 		System.out.print("태어난 년도를 입력하세요. ");
 		int birth_year = s.nextInt();
 		int age = cal(birth_year);
-	
+		print(age);
+		System.out.print("지금 나이는 " + age + "살 입니다.");
 	}
 
 	int cal(int birth_year){
 		int age = 0;
 		age = 2016 - birth_year + 1;
 		return age;
+
 	}
 
 	void print(int age){
-		if(age > 20)
-			System.out.print("미성년자가 아닙니다.");
-		
+		if(age > 19)
+			System.out.println("미성년자가 아닙니다.");
 		else 
-			System.out.print("미성년자 입니다.");
+			System.out.println("미성년자 입니다.");
 		
 	}
 
 	public static void main(String[] args){
 		new A01().input();
-		new A01().print(int age);
+		
+		
 	}
 
 
